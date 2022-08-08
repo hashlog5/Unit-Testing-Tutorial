24ffe9e6272b74d51d0557654314ff7acfcb830c
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-07 20:16:59 -0700

    write a unit test challenge

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class User {
   constructor(data = {}) {
     this.firstName = data.firstName || '';
     this.lastName = data.lastName || '';
-    this.middleName = data.middleName;
+    this.middleName = data.middleName || '';
   }
 }
 
@@ -14,14 +14,38 @@ class User {
  * Test Suite
  */
 describe(`${User.name} Class`, () => {
-  it('first name defaults to empty', () => {
-    // arrange
-    const data = { firstName: null };
+  describe('default values', () => {
+    it('first name defaults to empty', () => {
+      // arrange
+      const data = { firstName: null };
 
-    // act
-    const model = new User(data);
+      // act
+      const model = new User(data);
 
-    // assert
-    expect(model.firstName).toBe('');
+      // assert
+      expect(model.firstName).toBe('');
+    });
+
+    it('last name defaults to empty', () => {
+      // arrange
+      const data = { lastName: null };
+
+      // act
+      const model = new User(data);
+
+      // assert
+      expect(model.lastName).toBe('');
+    });
+
+    it('middle name defaults to empty', () => {
+      // arrange
+      const data = { middleName: null };
+
+      // act
+      const model = new User(data);
+
+      // assert
+      expect(model.middleName).toBe('');
+    });
   });
 });
