d3ec36ebb146135bda29726229f92477c40bcd97
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-07 19:57:35 -0700

    testing setup

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-describe('Test Example', () => {
-  it('returns true', () => {
-    expect(false).toBe(true);
+function insertDashes(str) {
+  //code
+}
+
+/**
+Test Guide
+**/
+describe('insertDashes()', () => {
+  it('insert dashes in between chars', () => {
+    //arrange
+    const value = 'aba caba';
+
+    // act
+    const result = insertDashes(value);
+
+    // log
+    console.log(('result:', result));
+
+    // assert
+    expect(result).toBe('a-b-a c-a-b-a');
   });
 });
